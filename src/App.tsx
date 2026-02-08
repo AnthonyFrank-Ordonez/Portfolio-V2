d227bcb4f3ad151bf87053554586f91d3fb342b9
AnthonyFrank-Ordonez <anthonyfrankordonez@yahoo.com>
2026-02-08 21:47:48 +0800

    test: testing husky pre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
